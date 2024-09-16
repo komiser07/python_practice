efa54c88631b3245f24d379512d8f576e31de34e
Mihail Komiser <komiser@gmail.com>
2024-09-16 22:20:17 +0300

    Initial commit

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
 print("Результат умножения: ", num1 * num2)
 
-num1 = int(input("Введите первое число: "))
-num2 = int(input("Введите второе число: "))
 try:
+    num1 = int(input("Введите первое число: "))
+    num2 = int(input("Введите второе число: "))
     print("Результат деления: ", num1 / num2)
+except ValueError:
+    print("Ошибка: введено не число!")
 except ZeroDivisionError:
     print("Ошибка деления на ноль!")
+finally:
+    print("Завершение работы программы.")
