4faf66e2c06f783149dc839a46001d473949c31b
Mihail Komiser <komiser@gmail.com>
2024-09-19 22:46:16 +0300

    Initial commit

--- Keywords.py
+++ Keywords.py
@@ -14,26 +14,25 @@ b = int(input("Введите второе число:"))
 
 x = max_of_two_num(a, b)
 
-print("Максимальное число из", a, "и", b, "это", x)
-
 
 def empty_function():
     pass
 
 
 def even_nums(n):
-    i = 0
     for i in range(n):
         if i % 2 == 0:
             yield i
 
 
-print(list(even_nums(n=int(input("Введите число n:")))))
-
-
 def test_max_of_two_num():
-    assert max_of_two_num(a, b) == x, "Ошибка: неправильное значение для различных пар"
+    assert max_of_two_num(57, 34) == 57, "Ошибка: неправильное значение для различных пар"
+    assert max_of_two_num(-1, 1) == 1, "Ошибка: неправильное значение для различных пар"
+    assert max_of_two_num(65, 64) == 65, "Ошибка: неправильное значение для различных пар"
 
 
 test_max_of_two_num()
+
+print("Максимальное число из", a, "и", b, "это", x)
+print(list(even_nums(int(input("Введите число n:")))))
 print("Все тесты пройдены!")
