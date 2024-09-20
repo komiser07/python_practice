9f07efe022aead0d909a78b33065a7bd6edad54d
Mihail Komiser <komiser@gmail.com>
2024-09-20 20:58:52 +0300

    Initial commit

--- Checking your password.py
+++ Checking your password.py
@@ -1,10 +1,17 @@
 secret_password = 'ПАРОЛЬ'
+
+attempts = 0
 while True:
+    attempts += 1
     password = input("Введите пароль: ")
 
     if password == secret_password:
-        print(f"Пароль верен!")
+        print(f"Пароль верен! Попыток: {attempts}")
+        break
+    elif attempts > 3:
+        print("Достигнуто максимальное количество попыток входа! Доступ заблокирован.")
+        break
     else:
-        print(f"Неверный пароль. Попробуйте еще раз.")
+        print(f"Неверный пароль. Попробуйте еще раз. Осталось {3 - attempts} попыток.")
 
 print("Добро пожаловать!")
