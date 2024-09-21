a496efae36f373ddcff994729a6569cdb2594e52
Mihail Komiser <komiser@gmail.com>
2024-09-21 19:11:23 +0300

    Initial commit

--- Adding list elements.py
+++ Adding list elements.py
@@ -1,13 +1,13 @@
 first_list = list(map(int, input("Введите первый список через пробел: ").split()))
 second_list = list(map(int, input("Введите второй список через пробел: ").split()))
 
-if len(first_list) != len(second_list):
-    print("Списки должны иметь одинаковую длину.")
-    exit()
-
 new_list = []
 for i in range(len(first_list)):
     new_list.append(first_list[i] + second_list[i])
+
+if len(first_list) != len(second_list):
+    print("Списки должны иметь одинаковую длину.")
+    exit()
 
 print("Результат: ", end="")
 for num in new_list:
