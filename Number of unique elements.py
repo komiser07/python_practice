2a2dbe103da07abae8f42d995c216b8c01707158
Mihail Komiser <komiser@gmail.com>
2024-09-22 11:19:12 +0300

    Initial commit

---
+++ Number of unique elements.py
@@ -0,0 +1,6 @@
+data = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 8]
+
+unique_elements = set(data)
+print(f"Количество уникальных элементов: {len(unique_elements)}")
+
+print("Уникальные элементы:", unique_elements)
