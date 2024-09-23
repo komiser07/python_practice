bdf19cc7d4c6db2fb7797dde0ff7264cfa1f250c
Михаил Сергеевич <komiser10@gmail.com>
2024-09-23 17:29:35 +0300

    Initial commit

---
+++ Checking for book availability.py
@@ -0,0 +1,84 @@
+library = {}
+
+
+def add_book(title, author, year):
+    if title in library:
+        choice = input("Книга '" + title + "' уже существует. Обновить информацию? (Y/N): ")
+        if choice.upper() == 'Y':
+            book_details = library[title]
+            library[title] = {"author": author, "year": year, "availability": book_details["availability"]}
+            print(f"Информация о книге '{title}' обновлена.\n")
+        else:
+            print("Отменено обновление.")
+    else:
+        library[title] = {"author": author, "year": year, "availability": None}
+        print(f"Книга '{title}' успешно добавлена.\n")
+
+
+def find_book(title):
+    if title in library:
+        print("Поиск книг:")
+        print(f"Title: {title}")
+        print(f"Author: {library[title]['author']}")
+        print(f"Year of publication: {library[title]['year']}")
+        print(f"Availability: {library[title]['availability']}\n")
+        if library[title]['availability'] == 'в наличии':
+            print(f"Книга '{title}' доступна\n")
+        elif library[title]['availability'] == 'выдана':
+            print(f"Книга '{title}' выдана\n")
+        else:
+            print(f"Книга '{title}' в библиотеке, но ее статус не определен\n")
+    else:
+        print(f"Книга '{title}' не найдена.\n")
+
+
+def issue_book(title):
+    if title in library:
+        if library[title]['availability'] == 'выдана':
+            print(f"Книга '{title}' уже выдана.\n")
+        else:
+            library[title]['availability'] = 'выдана'
+            print(f"Книга '{title}' успешно выдана.\n")
+    else:
+        print(f"Книга '{title}' не найдена.")
+
+
+def return_book(title):
+    if title in library:
+        if library[title]['availability'] == 'в наличии':
+            print(f"Книга '{title}' доступна к выдаче.\n")
+        else:
+            library[title]['availability'] = "в наличии"
+            print(f"Книга '{title}' успешно возвращена.\n")
+    else:
+        print(f"Книга '{title}' не найдена.")
+
+
+def remove_book(title):
+    if title in library:
+        del library[title]
+        print(f"Книга '{title}' успешно удалена.\n")
+    else:
+        print(f"Книга '{title}' не найдена.\n")
+
+
+def display_books():
+    for title, book_details in library.items():
+        print(f"Title: {title}")
+        print(f"Author: {book_details['author']}")
+        print(f"Year of publication: {book_details['year']}")
+        print(f"Availability: {book_details['availability']}\n")
+
+
+add_book("Путешествия Гулливера", "Джонатан Свифт", 1726)
+add_book("Робинзон Крузо", "Даниэль Дефо", 1719)
+add_book("Преступление и наказание", "Фёдор Достоевский", 1866)
+add_book("Думай и богатей", "Наполеон Хилл", 1937)
+
+display_books()
+issue_book("Путешествия Гулливера")
+find_book("Путешествия Гулливера")
+find_book("Робинзон Крузо")
+find_book("Искусство войны")
+
+display_books()
