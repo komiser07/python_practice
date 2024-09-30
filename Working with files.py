ab3afccecb2a27d439d4065b33761de66bb1a9af
Mihail Komiser <komiser@gmail.com>
2024-09-30 19:16:41 +0300

    Initial commit

---
+++ Working with files.py
@@ -0,0 +1,20 @@
+with open('data.txt', 'w') as file:
+    file.write("Доброе утро\n")
+    file.write("Добрый день\n")
+    file.write("Добрый вечер\n")
+
+with open('data.txt', 'r') as file:
+    print(file.read())
+
+with open('data.txt', 'a') as file:
+    file.write("Доброй ночи\n")
+    file.write("Пора вставать\n")
+
+with open('data.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        print(line.strip())
+
+with open('data.txt', 'rb') as source:
+    with open('copy_of_data.txt', 'wb') as destination:
+        destination.write(source.read())
