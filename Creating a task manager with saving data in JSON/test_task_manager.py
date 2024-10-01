efc0915ff81b26f05dab8c3657b38c3d61c7b1c9
Mihail Komiser <komiser@gmail.com>
2024-10-01 11:13:50 +0300

    Initial commit

--- Creating a task manager with saving data in JSON/test_task_manager.py
+++ Creating a task manager with saving data in JSON/test_task_manager.py
@@ -10,20 +10,18 @@ class TestTaskManager(unittest.TestCase):
         self.task_manager.add_task("выполнить задачу ")
         self.task_manager.complete_task(0)
         completed_task = self.task_manager._tasks[0]
-        self.assertEqual(completed_task["completed"], True)
+        self.assertTrue(self.task_manager._tasks[0]['completed'])
 
     def test_remove_task(self):
         self.task_manager.remove_task(0)
-        self.assertEqual(len(self.task_manager._tasks), 0)
+        self.assertFalse(self.task_manager._tasks)
 
     def test_save_and_load_json(self):
         self.task_manager.add_task("выполнить задачу ")
         self.task_manager.save_to_json("tasks.json")
         loaded_task_manager = TaskManager()
         loaded_task_manager.load_from_json("tasks.json")
-        saved_tasks = self.task_manager._tasks
-        loaded_tasks = loaded_task_manager._tasks
-        self.assertCountEqual(saved_tasks, loaded_tasks)
+        self.assertEqual(self.task_manager._tasks, loaded_task_manager._tasks)
 
 
 if __name__ == "__main__":
